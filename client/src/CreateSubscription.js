bc533f7fdf9a8c17945b3749b634f7db755e5ed3
Ponnuru Sai Venkatesh <saivenkat299911@gmail.com>
2024-05-28 18:25:35 +0530

    Updated API endpoints

--- client/src/CreateSubscription.js
+++ client/src/CreateSubscription.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Assuming you create and import App.css for styling
 
-require('dotenv').config();
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 
 
